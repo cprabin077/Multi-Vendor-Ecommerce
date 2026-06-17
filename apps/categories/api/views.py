8ac36d88758cf3f953c25e446402284597e2af64
= <kushmiprabin123@gmail.com>
2026-06-17 14:08:27 +0545

    fix: small issues

--- apps/categories/api/views.py
+++ apps/categories/api/views.py
@@ -1,54 +1,57 @@
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from apps.categories.api.serializer import CategorySerializer
 from apps.categories.models import Category
-# from rest_framework.permissions import IsAuthenticated
 
 
+# LIST + CREATE
 class CategoryView(GenericAPIView):
-    queryset = Category.objects.all() 
+    queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
-
     def get(self, request):
-        category = Category.objects.all()
-        serializer = CategorySerializer(category, many = True)
-        return Response(serializer.data, 200)
-    
+        categories = self.get_queryset()
+        serializer = self.serializer_class(categories, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
     def post(self, request):
-        data = request.data
-        serializer = CategorySerializer(data=data)
+        serializer = self.serializer_class(data=request.data)
+
         if serializer.is_valid():
             serializer.save()
-            return Response({"message": "Category Successfully created"}, 201)
-        else:
-            return Response(serializer.errors, 422)
-        
-        
+            return Response(
+                {"message": "Category successfully created"},
+                status=status.HTTP_201_CREATED,
+            )
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 class CategoryUpdateAndDelete(GenericAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
+    def get_object(self, pk):
+        return Category.objects.get(id=pk)
 
-    def put(self,request,pk):
-        trainer = Category.objects.get(id=pk)
-        data = request.data
-        serializer = CategorySerializer(trainer, data =data)
+    def put(self, request, pk):
+        category = self.get_object(pk)
+        serializer = self.serializer_class(category, data=request.data)
+
         if serializer.is_valid():
             serializer.save()
-            return Response({
-                "message":"Category successfully updated!!"
-            })
-        else:
-            return Response(serializer.errors,422)
-        
-    def delete(self, request,pk):
-        category = Category.objects.filter(id=pk)
-        category.delete()
-        return Response({
-            "message":"Category successfully deleted!!"
-        },200)
-        
+            return Response(
+                {"message": "Category successfully updated"}, status=status.HTTP_200_OK
+            )
 
-        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        category = self.get_object(pk)
+        category.delete()
+
+        return Response(
+            {"message": "Category successfully deleted"}, status=status.HTTP_200_OK
+        )
