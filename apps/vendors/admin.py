e91d29a9314ac6e49eb026e876b7d749b97eae70
= <kushmiprabin123@gmail.com>
2026-06-19 14:11:45 +0545

    feat: create vendors

--- apps/vendors/admin.py
+++ apps/vendors/admin.py
@@ -1,3 +1,81 @@
 from django.contrib import admin
+from .models import Vendor
 
-# Register your models here.
+
+@admin.register(Vendor)
+class VendorAdmin(admin.ModelAdmin):
+    list_display = (
+        "store_name",
+        "user",
+        "email",
+        "phone_number",
+        "is_verified",
+        "is_active",
+        "created_at",
+    )
+
+    list_filter = (
+        "is_verified",
+        "is_active",
+        "created_at",
+    )
+
+    search_fields = (
+        "store_name",
+        "user__username",
+        "user__email",
+        "email",
+        "phone_number",
+    )
+
+    list_editable = (
+        "is_verified",
+        "is_active",
+    )
+
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )
+
+    fieldsets = (
+        (
+            "Vendor Information",
+            {
+                "fields": (
+                    "user",
+                    "store_name",
+                    "store_description",
+                    "store_logo",
+                )
+            },
+        ),
+        (
+            "Contact Information",
+            {
+                "fields": (
+                    "email",
+                    "phone_number",
+                    "address",
+                )
+            },
+        ),
+        (
+            "Status",
+            {
+                "fields": (
+                    "is_verified",
+                    "is_active",
+                )
+            },
+        ),
+        (
+            "Timestamps",
+            {
+                "fields": (
+                    "created_at",
+                    "updated_at",
+                )
+            },
+        ),
+    )
--- apps/vendors/models.py
+++ apps/vendors/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Vendor(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name="vendor_profile"
+    )
+
+    store_name = models.CharField(max_length=255, unique=True)
+    store_description = models.TextField(blank=True)
+
+    email = models.EmailField(blank=True)
+    phone_number = models.CharField(max_length=15)
+
+    store_logo = models.ImageField(upload_to="vendors/logos/", blank=True, null=True)
+
+    address = models.TextField(blank=True)
+
+    is_verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.store_name
