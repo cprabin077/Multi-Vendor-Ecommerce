8d8ff6629afe4c6ca78e7115dee148b04b33e755
= <kushmiprabin123@gmail.com>
2026-06-20 17:17:40 +0545

    feat: create cart and cart item

--- apps/cart/admin.py
+++ apps/cart/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import Cart, CartItem
 
-# Register your models here.
+
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    extra = 0
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "created_at",
+    )
+
+    search_fields = (
+        "user__username",
+        "user__email",
+    )
+
+    inlines = [CartItemInline]
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "cart",
+        "product",
+        "quantity",
+        "created_at",
+    )
+
+    list_filter = ("created_at",)
+
+    search_fields = ("product__name",)
--- apps/cart/models.py
+++ apps/cart/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from apps.products.models import Product
+
+
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="cart")
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Cart"
+
+    class Meta:
+        db_table = "cart"
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey('cart.Cart', on_delete=models.CASCADE, related_name="items")
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    quantity = models.PositiveIntegerField(default=1)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product.name
+
+    class Meta:
+        db_table = "cart_item"
