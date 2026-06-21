c989567a1de9df6c18d1c866079e44ac3a890313
= <kushmiprabin123@gmail.com>
2026-06-21 15:23:17 +0545

    feat: create payments

--- apps/payments/admin.py
+++ apps/payments/admin.py
@@ -1,3 +1,67 @@
 from django.contrib import admin
+from .models import Payment
 
-# Register your models here.
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "order",
+        "amount",
+        "payment_method",
+        "status",
+        "created_at",
+    )
+
+    list_filter = (
+        "payment_method",
+        "status",
+        "created_at",
+    )
+
+    search_fields = (
+        "transaction_id",
+        "pidx",
+        "order__id",
+    )
+
+    list_editable = ("status",)
+
+    readonly_fields = (
+        "status",
+        "created_at",
+        "updated_at",
+    )
+
+    fieldsets = (
+        (
+            "Order Information",
+            {
+                "fields": (
+                    "order",
+                    "amount",
+                )
+            },
+        ),
+        (
+            "Payment Information",
+            {
+                "fields": (
+                    "payment_method",
+                    "status",
+                    "transaction_id",
+                    "pidx",
+                )
+            },
+        ),
+        ("Location Details", {"fields": ("location",)}),
+        (
+            "Timestamps",
+            {
+                "fields": (
+                    "created_at",
+                    "updated_at",
+                )
+            },
+        ),
+    )
--- apps/payments/models.py
+++ apps/payments/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+from apps.orders.models import Order
+
+
+class PaymentMethods(models.TextChoices):
+    COD = "COD", "Cash On Delivery"
+    KHALTI = "KHALTI", "Khalti"
+
+
+class Status(models.TextChoices):
+    COMPLETED = "COMPLETED", "Completed"
+    PENDING = "PENDING", "Pending"
+    USER_CANCELLED = "USER_CANCELLED", "User Cancelled"
+
+
+class Payment(models.Model):
+    order = models.OneToOneField(
+        Order, on_delete=models.CASCADE, related_name="payment"
+    )
+
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    payment_method = models.CharField(max_length=20, choices=PaymentMethods.choices)
+
+    transaction_id = models.CharField(max_length=255, blank=True, null=True)
+
+    pidx = models.CharField(max_length=255, blank=True, null=True)
+
+    status = models.CharField(
+        max_length=30, choices=Status.choices, default=Status.PENDING
+    )
+
+    location = models.TextField(null=True, blank=True)
+
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+
+    def __str__(self):
+        return f"Payment #{self.id}"
+
+    class Meta:
+        db_table = "payment"
